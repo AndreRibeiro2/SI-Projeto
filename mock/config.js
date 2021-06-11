5e438fa87f6931bfaed836b94f1514d8ed56136d
AndreRibeiro2019 <56743827+AndreRibeiro2019@users.noreply.github.com>
2021-06-11 15:07:09 -0300

    Add files via upload

---
+++ mock/config.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    name: 'Calça Feminina Adidas',
+    price: 312.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image1.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 320.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image2.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 120.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image3.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 90.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image4.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 88.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image5.png'
+  },
+  {
+    name: 'Boné Masculino',
+    price: 20.00,
+    description: 'Descrição',
+    category: 'Acessórios',
+    image: '../../images/bone m/bone 1.jpg',
+  },
+  {
+    name: '',
+    price: '',
+    description: 'Descrição',
+    category: '',
+    image: '',
+  },
+  {
+    name: '',
+    price: '',
+    description: 'Descrição',
+    category: '',
+    image: '',
+  }
+];
+
+const menuItens = [
+  {
+    name: 'Home',
+    href: '',
+  },
+  {
+    name: 'Acessórios',
+    href: '../../pages/Menu/acessorios.html',
+  },
+  {
+    name: 'Calçados',
+    href: '../../pages/Menu/calcados.html',
+  },
+  {
+    name: 'Feminino',
+    href: '../../pages/Menu/fem.html',
+  },
+  {
+    name: 'Masculino',
+    href: '../../pages/Menu/masc.html',
+  },
+  {
+    name: 'Infantil',
+    href: '../../pages/Menu/infantil.html',
+  },
+  {
+    name: 'Inverno',
+    href: '../../pages/Menu/inverno.html',
+  },
+];
